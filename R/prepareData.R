010a755f8dc10562528a019f8da10b26d6c1edd1
andzajan <andzajan@gmail.com>
2018-11-27 11:53:08 +0000

    preareData function refactored and tests added

--- R/prepareData.R
+++ R/prepareData.R
@@ -8,75 +8,79 @@
 #' @param glogScaling T or F, applie glog transformation to the given data
 #' @param qc_label Label used for QC samples. If set to NULL, assumes that no QC samples are present in data set
 #' @param ignorelabel Label for samples which should be excluded from processed data
-#' @param checkNA removes rows, columns containing all NA's
+#' @param checkNA removes rows or columns containing all NA's, also will chek if only all QC or analytical samples contain missing values
 #' @return List of processed data table and RSD% per sample class
 #' @export
 
 
-prepareData <- function (Data, classes, blank="BLANK", PQN=F, mv_impute=T, glogScaling=T, qc_label="QC", ignorelabel="Removed", checkNA=T)
-{
-    shits <- NULL
-    if (!is.null(blank))
-    {
-      shits <- which(classes==blank)
+prepareData <- function (Data, classes, blank="BLANK", PQN=F, mv_impute=T, glogScaling=T, qc_label="QC", ignorelabel="Removed", checkNA=T){
+  
+    Data <- check_peak_matrix_orientation(peak_data = Data, classes = classes)
+    
+    rem_samples <- NULL
+    if (!is.null(blank)){
+      
+      rem_samples <- which(classes==blank)
     }
-      if (!is.null(ignorelabel))
-    {
-      shits <- append(shits,which(classes==ignorelabel))
-    }
-
-    if(length(shits)>0)
-    {
-      Data <- Data[,-c(shits)]
-      classes <- classes[-c(shits)]
+      
+    if (!is.null(ignorelabel)){
+        
+      rem_samples <- append(rem_samples,which(classes==ignorelabel))
     }
 
-    # Remove rows (features) with all NA's in QC sample or in analytical sample, this can happen if we process data from more than one batch.
-    if (checkNA==T)
-    {
+    if(length(rem_samples)>0){
+      
+      Data <- Data[,-c(rem_samples)]
+      classes <- classes[-c(rem_samples)]
+    }
+
+    # Remove rows (features) with all NA's in QC sample or in analytical sample. 
+    # It can happen if we process data from more than one batch.
+    
+    if (checkNA==T){
+      
       AllNa <- function (x) all(is.na(x))
 
-      if (is.null(qc_label))
-      {
+      if (is.null(qc_label)){
+        
         hits2 <- which(apply(Data,1,AllNa)==T)
-      } else
-      {
+      } else {
+        
         hits2 <- which(apply(Data[,classes==qc_label],1,AllNa)==T)
         hits2 <- append(hits2, which(apply(Data[,-c(which(classes==qc_label))],1,AllNa)==T))
         hits2 <- unique(hits2)
       }
 
-      if (length(hits2)>0)
-      {
+      if (length(hits2)>0){
+        
         Data <- Data[-c(hits2),]
       }
     }
+    
     RSD <- doRSD (Data=Data, classes = classes)
 
     #pqn normalisation
-    if (PQN==T)
-    {
+    if (PQN==T & !is.null(qc_label)){
       Data <- pqn_normalisation(df=Data, classes=classes, qc_label = qc_label)[[1]]
     }
 
-    if (mv_impute==T)
-    {
-      #impute missing values
-      vals <- mv_imputation(t(Data), 'knn', k=5, rowmax=1, colmax=1)
-      Data <- t(vals)
+    # mv imputation
+    if (mv_impute==T){
+      Data <- mv_imputation(Data, 'knn', k=5, rowmax=1, colmax=1)
     }
 
     #glog scaling
-    if (glogScaling==T)
-    {
-      Data <- glog_transformation(df=t(Data), classes=classes, qc_label=qc_label)
-      Data <- t(Data)
+    if (glogScaling==T & !is.null(qc_label)){
+      
+      Data <- glog_transformation(df=Data, classes=classes, qc_label=qc_label)
     }
 
     out <- list(Data, classes, RSD)
     names (out) <- c("Data","classes","RSD")
     out
 }
+
+
 
 
 
