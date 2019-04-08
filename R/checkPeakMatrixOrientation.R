83f0c356bd29d9beebab4d521c3863e9fe4b40ac
andzajan <andzajan@gmail.com>
2019-04-08 17:41:14 +0100

    checkPeakOrientation will keep data type data.frame if transpose is needed

--- R/checkPeakMatrixOrientation.R
+++ R/checkPeakMatrixOrientation.R
@@ -18,6 +18,8 @@
 check_peak_matrix_orientation <- function (peak_data, classes=NULL)
 {
   dims <- dim(peak_data)
+  is_data_frame <- is.data.frame(peak_data)
+  
   if (dims[1] < dims[2] & is.null(classes)){
     peak_data <- t(peak_data)
     warning ("Peak table was transposed to have features as rows and samples in columns. \n
@@ -27,21 +29,21 @@ check_peak_matrix_orientation <- function (peak_data, classes=NULL)
   }
   
   if (!is.null(classes)){
-    hits <- which(dims==length(classes))
-    if (length(hits)==2){
+    hits <- which(dims == length(classes))
+    if (length(hits) == 2){
       warning (" Number of samples and features is the same in your data matrix, 
                please make sure that you samples are in columns. \n")
-    }
-  
-    if (length(hits)==0){
+    } else if (length(hits) == 0){
       stop (" Length of sample classes doesn't match any dimension of input data. 
             Sample labels should match number of samples. \n")
-    }
-  
+    } else if (hits == 1){
     # If samples are in rows, transpose data matrix
-    if (hits==1){
       peak_data <- t(peak_data)
     }
+  }
+  
+  if (is_data_frame){
+    peak_data <- as.data.frame(peak_data)
   }
   
   peak_data
--- tests/testthat/test-checkpeakmatrixorientation.R
+++ tests/testthat/test-checkpeakmatrixorientation.R
@@ -1,34 +1,47 @@
 context("test-check_peakmatrix_orientation")
 
 test_that("Output data matrix is in correct orientation", {
-  out <- check_peak_matrix_orientation(peak_data =t(testData$data), classes = testData$class)
+  out <- check_peak_matrix_orientation(peak_data=t(testData$data), classes=testData$class)
   expect_equal(out, testData$data)
+})
+
+test_that("Output data matrix is in correct orientation and data frame type is returned", {
+  peak_data <- as.data.frame(t(testData$data))
+  out <- check_peak_matrix_orientation(peak_data=peak_data, classes=testData$class)
+  expect_true(is.data.frame(out))
 })
 
 test_that("Output data matrix is in correct orientation if no transpose is needed", {
-  out <- check_peak_matrix_orientation(peak_data =testData$data, classes = testData$class)
+  out <- check_peak_matrix_orientation(peak_data=testData$data, classes=testData$class)
   expect_equal(out, testData$data)
 })
 
 test_that("Function fails", {
-  expect_error(check_peak_matrix_orientation(peak_data =testData$data, classes=testData$class[1:3]))
+  expect_error(check_peak_matrix_orientation(peak_data=testData$data, classes=testData$class[1:3]))
 })
 
 test_that("Function fails", {
-  expect_error(check_peak_matrix_orientation(peak_data =testData$data[,1:6], classes=testData$class))
+  expect_error(check_peak_matrix_orientation(peak_data=testData$data[,1:6], classes=testData$class))
 })
 
 test_that("Function works if class labels are not provided", {
-  out <- check_peak_matrix_orientation (peak_data = testData$data)
+  out <- check_peak_matrix_orientation (peak_data=testData$data)
   expect_equal (out, testData$data)
 })
 
 test_that("Function works if class labels are not provided and matrix needs to be transposed", {
-  expect_warning (out <- check_peak_matrix_orientation (peak_data = t(testData$data)))
+  expect_warning (out <- check_peak_matrix_orientation (peak_data=t(testData$data)))
   expect_equal(out, testData$data)
 })
 
+test_that("Function works if class labels are not provided and matrix needs to be transposed,
+          and data type is retained", {
+  peak_data <- data.frame(t(testData$data))
+  expect_warning (out <- check_peak_matrix_orientation (peak_data=peak_data))
+  expect_true(is.data.frame(out))
+})
+
 test_that("Function returns warning if input peak matrix has the same number of samples and features", {
-  expect_warning (out <- check_peak_matrix_orientation (peak_data = testData$data[1:9, ], 
-                                                        classes = testData$class))
+  expect_warning (out <- check_peak_matrix_orientation (peak_data=testData$data[1:9, ], 
+                                                        classes=testData$class))
 })
