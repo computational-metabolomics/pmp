6d47c3e671ee8392a6bce27ac9042144de8e3f8e
andzajan <andzajan@gmail.com>
2019-06-27 21:50:54 +0100

    Added tests to check another two special cases in glog transformation

--- tests/testthat/test-glog_transformation.R
+++ tests/testthat/test-glog_transformation.R
@@ -2,17 +2,28 @@ context("test-glog_transformation")
 
 test_that("Glog function returns expected output", {
   out <- mv_imputation(df=testData$data, method = "knn")
-  expect_equal (glog_transformation (df=out, classes = testData$class, qc_label = "QC"), testData$glog_transformation)
+  expect_equal (glog_transformation (df=out, classes = testData$class, 
+    qc_label = "QC"), testData$glog_transformation)
 })
 
 test_that("Glog function fails if qc_label is wrong or QC samples don't exist", {
   out <- mv_imputation(df=testData$data, method = "knn")
-  expect_error (glog_transformation (df=out, classes = testData$class, qc_label = "A"))
+  expect_error (glog_transformation (df=out, classes = testData$class, 
+    qc_label = "A"))
 })
 
 test_that("Scale factor is applied if optimisation fails", {
     df=iris
     classes=iris$Species
     df=df[,-5]
-    expect_output(glog_transformation(df,classes,qc_label='versicolor'),regexp = 'Error!Lambda')
+    expect_output(glog_transformation(df,classes,qc_label='versicolor'), 
+      regexp = 'Error!Lambda')
+})
+
+test_that("If feature variance is 0 replace it with small value", {
+    out <- mv_imputation(df=testData$data, method = "knn")
+    out[3,] <- 3
+    expect_output(out <- glog_transformation (df=out, classes = testData$class, 
+      qc_label = "QC")[3,], regexp = 'Error!Lambda')
+    testthat::expect_true(all(out[1:3]==out[4:6]))
 })
