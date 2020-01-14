d3fe7d141a1b721c9ac6d200ecf0ad3ee3d84bf0
andzajan <andzajan@gmail.com>
2020-01-14 20:06:22 +0000

    Removed reference to sbcms Namespace

--- tests/testthat/test-splineSmoother.R
+++ tests/testthat/test-splineSmoother.R
@@ -6,8 +6,8 @@ x_qc <- c(1, 2, 3, 7, 13, 18, 23)
 x_sample <- c(4, 5, 6, 8, 9, 10, 11, 12, 14, 15, 16, 17, 19, 20, 21, 22)
 
 test_that ("splineSmoother returns expected output", {
-  out <- sbcms:::splineSmoother(x=x_qc, y=y_qc, newX=x_sample,
-    log=TRUE, spar=0, a=1, verbose=T)
+  out <- splineSmoother(x=x_qc, y=y_qc, newX=x_sample,
+    log=TRUE, spar=0, a=1)
   expect_equal (out, testData$splineSmoother)
 
 })
